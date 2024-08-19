b25d1d439c1a2d8cf448362c48b104f9689e087f
programadordostas <enrique2tas@gmail.com>
2024-08-19 13:57:19 -0500

    Ancho-sidebar

--- src/app/sidebar/sidebar-main/sidebar-main.component.ts
+++ src/app/sidebar/sidebar-main/sidebar-main.component.ts
@@ -4,7 +4,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar-main',
   templateUrl: './sidebar-main.component.html',
-  styleUrl: './sidebar-main.component.css',
+  styleUrl: './sidebar-main.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidebarMainComponent { }
